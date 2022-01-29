e2e5df5f026c4bac87734ebd5f021122f46f14a5
moulijhunjhunwala <63231240+moulijhunjhunwala@users.noreply.github.com>
2022-01-29 18:10:20 +0530

    Add files via upload

---
+++ checking.js
@@ -0,0 +1,13 @@
+
+const number = parseInt(prompt("Enter a number: "));
+if (number > 0) {
+    console.log(" positive");
+}
+else if (number == 0) {
+  console.log(" zero");
+}
+
+else {
+     console.log(" negative");
+}
+
